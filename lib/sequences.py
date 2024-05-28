e66a89a126c0e06598ed2dcf3f6c7955e774b1bc
jayson <jaysonshammah3@gmail.com>
2024-05-28 10:25:30 +0300

    complete sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -2,3 +2,17 @@
 
 def print_fibonacci(length):
     pass
+
+    if length <= 0 or len(str(length)) == 0:
+        print([])
+        return
+
+    result = []
+    a, b = 0, 1
+    for i in range(length):
+        result.append(a)
+        a, b = b, b+a
+
+    print(result)
+
+print_fibonacci(0)
